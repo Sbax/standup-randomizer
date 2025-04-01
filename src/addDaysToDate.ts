62f4d1d608e51ac2139a1f36917da95d3076e5bc
Matteo Bacci <matteo.bacci@nearform.com>
2025-04-01 19:06:57 +0200

    added date-fns

--- src/addDaysToDate.ts
+++ src/addDaysToDate.ts
@@ -1,42 +1,14 @@
+import { addDays, format, isWeekend } from "date-fns";
+
 export function addDaysToDate(startDate: string, count: number): string {
-  const daysOfWeek = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
+  const calculateDate = (date: Date, remainingDays: number): Date => {
+    if (remainingDays === 0) return date;
+    const nextDate = addDays(date, 1);
+    return isWeekend(nextDate)
+      ? calculateDate(nextDate, remainingDays)
+      : calculateDate(nextDate, remainingDays - 1);
+  };
 
-  const date = new Date(startDate);
-
-  for (let addedDays = 0; addedDays < count; ) {
-    date.setDate(date.getDate() + 1);
-    const dayOfWeek = date.getDay();
-
-    if (dayOfWeek !== 0 && dayOfWeek !== 6) {
-      addedDays++;
-    }
-  }
-
-  const dayName = daysOfWeek[date.getDay()];
-  const monthName = months[date.getMonth()];
-  const day = date.getDate();
-
-  return `${dayName} ${monthName} ${day}`;
+  const finalDate = calculateDate(new Date(startDate), count);
+  return format(finalDate, "EEEE MMMM d");
 }
